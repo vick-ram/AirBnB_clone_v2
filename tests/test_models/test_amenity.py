06aaec49646b6c85f0f06ffad4a16e42f9b32f82
vick-ram <vickramodero6@gmail.com>
2024-04-17 07:06:54 -0700

    Added some files

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
-""" """
+"""
+This module tests for amenity model class
+"""
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
 
 
 class test_Amenity(test_basemodel):
-    """ """
+    """This class initializes the test and it implements test_basemodel
+    super class
+    """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """This function creates an initialization of test_amenity class
+	It takes two arguments args and kwargs
+	"""
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
     def test_name2(self):
-        """ """
+        """This function tests for name and returns true if string type
+	"""
         new = self.value()
         self.assertEqual(type(new.name), str)
