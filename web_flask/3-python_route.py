2fe407533d9bf26aa91a106e4d6f431279045787
vick-ram <vickramodero6@gmail.com>
2024-05-22 09:08:30 -0700

    path parameter with default

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Receive a path variable"""
+from flask import Flask, redirect
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Hello function"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """HBNB funtion"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """Function to return a text with passed variable"""
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text='is cool'):
+    """Function to display text passed, has default defined"""
+    return f"Python {text.replace('_', ' ')}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
