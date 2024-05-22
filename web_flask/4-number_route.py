f045d2936de7933b293e345a686d2efa6d9dc3af
vick-ram <vickramodero6@gmail.com>
2024-05-22 09:14:55 -0700

    number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Receive a path variable"""
+from flask import Flask, redirect
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Hello function"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """HBNB funtion"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """Function to return a text with passed variable"""
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text='is cool'):
+    """Function to display text passed, has default defined"""
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def int_route(n):
+    """integer path param"""
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
