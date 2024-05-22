4f22a237d9411b6ba1cefdde9113c1435b6afc38
vick-ram <vickramodero6@gmail.com>
2024-05-22 09:59:38 -0700

    code refactore on storage and model classes

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -71,3 +71,7 @@ class DBStorage:
         )
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+    def close(self):
+        """closes the working SQLAlchemy session"""
+        self.__session.close()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -64,3 +64,7 @@ class FileStorage:
             key = "{}.{}".format(obj.__class__.__name__, obj.id)
             if key in FileStorage.__objects:
                 del FileStorage.__objects[key]
+
+    def close(self):
+        """Call the reload method"""
+        self.reload()
